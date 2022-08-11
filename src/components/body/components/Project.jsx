c4c4ff38a503a8f01a01d1577e6a88bf81768bc1
Huyhehe <thhyy2002@gmail.com>
2022-08-11 23:36:28 +0700

    config content of projects - project section

--- src/components/body/components/Project.jsx
+++ src/components/body/components/Project.jsx
@@ -5,10 +5,10 @@ const Project = () => {
     {
       title: "Guitar Shop Management Website",
       description:
-        "This project was built for a guitar shop to manage employees and commodities easier and can cut down the manage-fee",
+        "This project was built for a guitar shop to manage employees and commodities easier and can cut down the manage-fee. Actually my team used VueJS(V2) for frontend and .NET CORE for backend. This website's just a private website for specific guitar shop that using it for management, then may be you can not register for using",
       cover: guitarshop,
       href: "https://pbl-3-dcur38hyc-huyhehe.vercel.app/login",
-      since: "1 year",
+      since: "May 2022 - July 2022",
     },
     {
       title: "CV Sample",
@@ -16,33 +16,33 @@ const Project = () => {
         "This one just an HTML&CSS cut from random figma project on Figma Community",
       cover: due,
       href: "https://huyhehe.github.io/landing-page/",
-      since: "1 year",
+      since: "Within one day - July 22 2022",
     },
     {
       title: "Lasles VPN",
       description:
-        "This one just an HTML&CSS cut from random figma project on Figma Community",
+        "This one is the first project of mine with HTML&CSS, cut from random figma project on Figma Community",
       cover: lasles,
       href: "https://huyhehe.github.io/figma1/",
-      since: "1 year",
+      since: "October 2021 - November 2021",
     },
     {
       title: "Clone Twitter",
       description:
         "This project clone from Twitter about UI UX and some basic CRUD working behind, for now this project still on development",
       cover: twitter,
-      href: "https://huyhehe.github.io/twitter-clone/",
-      since: "1 year",
+      href: "http://twitter-clone-huyhehe.surge.sh/",
+      since: "July 2022 - Recently",
     },
   ];
   return (
     <>
       <div className="project-wrapper flex flex-col items-center py-[7rem]">
-        <div className="flex flex-col gap-[1rem] items-center text-textPrimary">
+        <div className="flex flex-col gap-[0.75rem] items-center text-textPrimary mb-[3rem]">
           <h1 className="w-fit text-[3rem] font-bold roboto-mono">
             My projects
           </h1>
-          <p className="text-[1.25rem]">
+          <p className="text-[1.25rem] text-textSecondary font-medium italic">
             These projects down below are the results of my frontend studying
             through 1 year
           </p>
@@ -63,7 +63,7 @@ const Project = () => {
                     />
                     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full opacity-0 bg-white group-hover:opacity-20 transition-all"></div>
                   </div>
-                  <div className="absolute top-1/2 left-1/2 -translate-x-1/2 translate-y-1/2 group-hover:-translate-y-1/2 w-full h-full transition-all group-hover:delay-300 duration-300 bg-bgSecondary rounded-md flex flex-col gap-[1rem] p-[2rem] font-bold text-textPrimary">
+                  <div className="absolute top-1/2 left-1/2 -translate-x-1/2 translate-y-1/2 group-hover:-translate-y-1/2 w-full h-full transition-all group-hover:delay-300 duration-300 bg-bgSecondary rounded-md flex flex-col gap-[1rem] p-[2rem] font-bold text-textPrimary overflow-auto">
                     <span className=" text-textSecondary">{project.since}</span>
                     <span className="text-[1.5rem]">{project.title}</span>
                     <p className="text-[0.85rem] font-medium">
